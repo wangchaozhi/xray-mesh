6c5b3702876b2d91b771340d78a359b52c2ce049
王超智 <50672959+wangchaozhi@users.noreply.github.com>
2026-09-14 14:27:44 +0800

    feat: expose discovery configuration on unsupported platforms

--- internal/tun/unsupported.go
+++ internal/tun/unsupported.go
@@ -3,9 +3,12 @@
 package tun
 
 import (
+	"context"
 	"errors"
 )
 
 var ErrUnsupported = errors.New("TUN implementation is currently available only on Linux")
 
 func Open(string) (Device, error) { return nil, ErrUnsupported }
+
+func ConfigureDiscovery(context.Context, Device) error { return ErrUnsupported }
